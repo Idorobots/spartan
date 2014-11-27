25e8346aba43b885f22e8c1f1ad2ac64688c9e09
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2014-11-27 22:34:41 +0100

    Added compiler stub.

---
+++ src/bootstrap/bootstrap.scm
@@ -0,0 +1,35 @@
+;; Scheme bootstrap code
+
+(define true #t)
+
+(define false #f)
+
+(define nil '())
+
+(define (id x) x)
+
+(define (empty? lst)
+  (null? lst))
+
+(define old-gensym gensym)
+(define (gensym root)
+  (old-gensym (symbol->string root)))
+
+(define (slurp file-name)
+  (with-input-from-file file-name
+    (lambda ()
+      (list->string
+       (reverse (let loop ((char (read-char))
+                           (result '()))
+                  (if (eof-object? char)
+                      result
+                      (loop (read-char) (cons char result)))))))))
+
+(define (parse file)
+  (with-input-from-string (string-append "(do" file ")")
+    (lambda () (read))))
+
+(use-modules (ice-9 pretty-print))
+(define old-pp pretty-print)
+(define (pretty-print expr)
+  (old-pp expr))
