9fe77bfd3d212b4e2870c161e0a8f0fa22844359
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-07-20 21:01:39 +0200

    Added the main entry point to the prototype.

---
+++ src/main.scm
@@ -0,0 +1,27 @@
+;; The main entry point.
+
+(load "compiler/compiler.scm")
+(load "compiler/parser.scm")
+
+(define (do-expr expr)
+  (eval/pp (compile/pp expr)))
+
+(define (do-string str)
+  (eval/pp (compile/pp (parse str))))
+
+(define (do-file filename)
+  (eval/pp (compile/pp (parse (slurp filename)))))
+
+(define (compile/pp expr)
+  (display ";; Code:")
+  (newline)
+  (pretty-print expr)
+  (compile expr))
+
+(define (eval/pp expr)
+  (display ";; Compiled code:")
+  (newline)
+  (pretty-print expr)
+  (display ";; Result:")
+  (newline)
+  (eval expr))
