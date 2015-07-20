042546e51fee150fe53d6973d6ca8bf6fa2a166d
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-07-21 01:15:04 +0200

    Added more runtime tests.

--- test/rt/resume.scm
+++ test/rt/resume.scm
@@ -11,3 +11,8 @@
 
 ;; Can as easily resume stuff.
 (assert (resume (resume (resume (do-expr '(= (* 3 2) (+ 3 3)))))))
+
+;; Can do the same for do-string
+(assert (run (do-string "23")) 23)
+(assert (run (do-string "(= (* 3 2) (+ 3 3))")))
+(assert (resume (resume (resume (do-string "(= (* 3 2) (+ 3 3))")))))
