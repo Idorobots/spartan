a2732b51a7fae979cda1cd643e24c4d91ec708a7
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-07-20 20:48:37 +0200

    Move parsing to a separate file.

---
+++ src/compiler/parser.scm
@@ -0,0 +1,15 @@
+;; A very simple parser.
+
+(define (slurp file-name)
+  (with-input-from-file file-name
+    (lambda ()
+      (list->string
+       (reverse (let loop ((char (read-char))
+                           (result '()))
+                  (if (eof-object? char)
+                      result
+                      (loop (read-char) (cons char result)))))))))
+
+(define (parse file)
+  (with-input-from-string (string-append "(do" file ")")
+    (lambda () (read))))
--- src/compiler/utils.scm
+++ src/compiler/utils.scm
@@ -22,22 +22,6 @@
 (define (deref x)
   x)
 
-;; IO:
-
-(define (slurp file-name)
-  (with-input-from-file file-name
-    (lambda ()
-      (list->string
-       (reverse (let loop ((char (read-char))
-                           (result '()))
-                  (if (eof-object? char)
-                      result
-                      (loop (read-char) (cons char result)))))))))
-
-(define (parse file)
-  (with-input-from-string (string-append "(do" file ")")
-    (lambda () (read))))
-
 ;; Other stuff
 
 (define-syntax do
