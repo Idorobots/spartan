dd4a26cf310a340285dadf66a67f48a16e1cb3ff
Kajetan Rzepecki <k@spartan.works>
2021-02-23 22:02:07 +0100

    Converted the parser tests.

--- test/compiler/parser.scm
+++ test/compiler/parser.scm
@@ -1,33 +1,27 @@
 ;; Parser tests.
 
-(assert (parse "foo") 'foo)
-(assert (parse "(define (foo x) 23)") '(define (foo x) 23))
-(assert (parse "(define (oof x) 32)") '(define (oof x) 32))
-(assert (parse "(define (foo x) ;; Coments should be removed!
+(describe
+ "parser"
+ (it "parses simple expressions"
+     (assert (parse "foo") 'foo)
+     (assert (parse "(define (foo x) 23)") '(define (foo x) 23))
+     (assert (parse "(define (oof x) 32)") '(define (oof x) 32)))
+ (it "parses comments"
+     (assert (parse "(define (foo x) ;; Coments should be removed!
                    true)")
-        '(define (foo x) true))
-
-(define (expected-read input)
-  (with-input-from-string input
-    (lambda ()
-      (read))))
-
-(map (lambda (filename)
-       (let ((contents (slurp filename)))
-         (assert (parse contents)
-                 (expected-read contents))))
-     (list
-      "../test/foof/hello.foo"
-      "../test/foof/fibonacci.foo"
-      "../test/foof/logger.foo"
-      "../test/foof/errors.foo"
-      "../test/foof/coroutines.foo"
-      "../test/foof/coroutines2.foo"
-      "../test/foof/uprocs.foo"
-      "../test/foof/uprocs2.foo"
-      "../test/foof/msgwait.foo"
-      "../test/foof/fibonacci2.foo"
-      "../test/foof/errors2.foo"
-      "../test/foof/rbs2.foo"
-      "../test/foof/rbs.foo"
-      "../test/foof/cep.foo"))
+             '(define (foo x) true)))
+ (it "parses all the examples"
+     (define (expected-read input)
+       (with-input-from-string input
+         (lambda ()
+           (read))))
+     (map (lambda (filename)
+            (let ((contents (slurp filename)))
+              (assert (parse contents)
+                      (expected-read contents))))
+          (filter (lambda (filename)
+                    (string-suffix? filename ".foo"))
+                  (map (lambda (path)
+                         (string-append "../test/foof/"
+                                        (path->string path)))
+                       (directory-list "../test/foof/"))))))
