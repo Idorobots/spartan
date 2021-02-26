50aa0e78c4680e0b127458568c422b48beacd67d
Kajetan Rzepecki <k@spartan.works>
2021-02-26 23:37:54 +0100

    Improved the parser performance tests.

--- test/performance.scm
+++ test/performance.scm
@@ -17,9 +17,11 @@
                          (iota 0 50 5))))
         (printf "~a, ~a, ~a, ~a~n" 'file-size 'cpu 'real 'gc)
         (map (lambda (input)
-               (collect-garbage)
+               (collect-garbage 'major)
                (let ((size (+ 1 (count (partial equal? #\newline) (string->list input))))
                      (time (time-execution (parse input))))
                  (apply printf "~a, ~a, ~a, ~a~n" size time)
-                 (car time)))
+                 ;; NOTE It's hard to avoid the GC here, so in case it kicks in anyway we
+                 ;; NOTE lie and deceive about the time it took to parse the file.
+                 (- (car time) (caddr time))))
              inputs)))))
