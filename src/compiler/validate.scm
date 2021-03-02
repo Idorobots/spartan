e38ee89301dac3063be3d3f05dc4fb81aa4f7f54
Kajetan Rzepecki <k@spartan.works>
2021-03-02 21:19:43 +0100

    Handle unmatched parens in the validation phase.

--- src/compiler/validate.scm
+++ src/compiler/validate.scm
@@ -5,30 +5,40 @@
 (load "compiler/tree-ast.scm")
 
 (define (validate expr input)
-  (with-handlers
-      ((syntax-error?
-        (lambda (error)
-          (let ((location (syntax-error-location error))
-                (what (syntax-error-what error))
-                (restart (syntax-error-restart error)))
-            (display (format-error input location what))
-            (newline)
-            ;; NOTE Continue analysis with a special "error" object.
-            (restart (at location
-                         (generated
-                          (make-error-node))))))))
-    (map-ast id
-             (lambda (expr)
-               (case (ast-get expr 'type 'undefined)
-                 ('unterminated-string (raise-syntax-error
-                                        expr
-                                        "Unterminated string literal - expected a closing `\"` to follow:"))
-                 ('unterminated-list (raise-syntax-error
-                                      expr
-                                      "Unterminated list - expected a closing `)` to follow:"))
-                 ('unterminated-quote (raise-syntax-error
-                                       expr
-                                       (format "No expression following `~a`:"
-                                               (ast-get expr 'value compiler-bug))))
-                 (else expr)))
-             expr)))
+  (let* ((errors (ref '()))
+         (result (with-handlers
+                     ((syntax-error?
+                       (lambda (error)
+                         (let* ((location (syntax-error-location error))
+                               (what (syntax-error-what error))
+                               (restart (syntax-error-restart error))
+                               (error (format-error input location what)))
+                           (display error)
+                           (newline)
+                           (assign! errors (cons error (deref errors)))
+                           ;; NOTE Continue analysis with a special "error" object.
+                           (restart (at location
+                                        (generated
+                                         (make-error-node))))))))
+                   (map-ast id
+                            (lambda (expr)
+                              (case (ast-get expr 'type 'undefined)
+                                ('unmatched-token (raise-syntax-error
+                                                   expr
+                                                   "Unmatched parentheses - expected an opening `(` to come before:"))
+                                ('unterminated-list (raise-syntax-error
+                                                     expr
+                                                     "Unterminated list - expected a closing `)` to follow:"))
+                                ('unterminated-string (raise-syntax-error
+                                                       expr
+                                                       "Unterminated string literal - expected a closing `\"` to follow:"))
+                                ('unterminated-quote (raise-syntax-error
+                                                      expr
+                                                      (format "No expression following `~a`:"
+                                                              (ast-get expr 'value compiler-bug))))
+                                (else expr)))
+                            expr))))
+    (if (empty? (deref errors))
+        result
+        ;; FIXME Properly stop the compiler pipeline.
+        (make-error-node))))
