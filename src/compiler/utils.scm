91f5a7609a752a83cc4a5e8426658bf2cb0b1ea4
Kajetan Rzepecki <k@spartan.works>
2021-03-13 15:07:26 +0100

    Added stack trace printing to the debug compiler error.

--- src/compiler/utils.scm
+++ src/compiler/utils.scm
@@ -100,5 +100,12 @@
 
 ;; Internal errors
 
+(define (show-stacktrace)
+  (for ([s (continuation-mark-set->context (current-continuation-marks))]
+        [i (in-naturals)])
+    ;; show just the names, not the full source information
+    (when (car s) (printf "~s: ~s\n" i s))))
+
 (define (compiler-bug)
+  (show-stacktrace)
   (error "Likely a compiler bug!"))
