55ee8536d5ce5202a2428915b4db47a8df2fed98
Kajetan Rzepecki <k@spartan.works>
2021-03-19 20:52:22 +0100

    Updated macro-expansion tests.

--- test/compiler/macros.scm
+++ test/compiler/macros.scm
@@ -2,41 +2,108 @@
 
 (define bm (make-builtin-macros))
 
+(define (l n) (location n n))
+
 (describe
  "built-in macros"
- (it "let macro works"
-     (assert (macro-expand '(let () (list a b c)) bm)
-             '((lambda () (list a b c))))
-     (assert (macro-expand '(let ((a 23)) b) bm)
-             '((lambda (a) b) 23)))
-
  (it "let* macro works"
-     (assert (macro-expand '(let* ((a 23) (b 5)) c) bm)
-             '((lambda (a) ((lambda (b) c) 5)) 23))
-     (assert (macro-expand '(let* ((a 23)) a) bm)
-             '((lambda (a) a) 23))
-     (assert (macro-expand '(let* () a) bm)
-             'a))
+     (assert (expand-macros (at (l 1) (make-list-node
+                                       (list (at (l 2) (make-symbol-node 'let*))
+                                             (at (l 3) (make-list-node '()))
+                                             (at (l 10) (make-symbol-node 'c)))))
+                            bm)
+             (at (l 10) (make-symbol-node 'c)))
+     (assert (expand-macros (at (l 1) (make-list-node
+                                       (list (at (l 2) (make-symbol-node 'let*))
+                                             (at (l 3) (make-list-node
+                                                        (list (at (l 4)
+                                                                  (make-list-node
+                                                                   (list (at (l 5) (make-symbol-node 'a))
+                                                                         (at (l 6) (make-number-node 23))))))))
+                                             (at (l 10) (make-symbol-node 'c)))))
+                            bm)
+             (at (l 1)
+                 (make-let-node
+                  (list (cons (at (l 5) (make-symbol-node 'a))
+                              (at (l 6) (make-number-node 23))))
+                  (at (l 10) (make-symbol-node 'c)))))
+     (assert (expand-macros (at (l 1) (make-list-node
+                                       (list (at (l 2) (make-symbol-node 'let*))
+                                             (at (l 3) (make-list-node
+                                                        (list (at (l 4)
+                                                                  (make-list-node
+                                                                   (list (at (l 5) (make-symbol-node 'a))
+                                                                         (at (l 6) (make-number-node 23)))))
+                                                              (at (l 7)
+                                                                  (make-list-node
+                                                                   (list (at (l 8) (make-symbol-node 'b))
+                                                                         (at (l 9) (make-number-node 5))))))))
+                                             (at (l 10) (make-symbol-node 'c)))))
+                            bm)
+             (at (l 1)
+                 (make-let-node
+                  (list (cons (at (l 5) (make-symbol-node 'a))
+                              (at (l 6) (make-number-node 23))))
+                  (at (l 1)
+                      (make-let-node
+                       (list (cons (at (l 8) (make-symbol-node 'b))
+                                   (at (l 9) (make-number-node 5))))
+                       (at (l 10) (make-symbol-node 'c))))))))
 
  (it "handle macro works"
-     (assert (macro-expand '(handle expr handler) bm)
-             '(call/handler handler
-                            (lambda () expr))))
+     (assert (expand-macros (at (l 1) (make-list-node
+                                       (list (at (l 2) (make-symbol-node 'handle))
+                                             (at (l 3) (make-symbol-node 'expr))
+                                             (at (l 4) (make-symbol-node 'handler)))))
+                            bm)
+             (at (l 1) (make-app-node (at (l 1) (make-symbol-node 'call/handler))
+                                      (list (at (l 4) (make-symbol-node 'handler))
+                                            (at (l 1)
+                                                (make-lambda-node '()
+                                                              (at (l 3) (make-symbol-node 'expr)))))))))
 
  (it "shift macro works"
-     (assert (macro-expand '(shift k (k 23)) bm)
-             '(call/shift
-               (lambda (k) (k 23)))))
+     (assert (expand-macros (at (l 1) (make-list-node
+                                       (list (at (l 2) (make-symbol-node 'shift))
+                                             (at (l 3) (make-symbol-node 'kont))
+                                             (at (l 4) (make-symbol-node 'expr)))))
+                            bm)
+             (at (l 1) (make-app-node (at (l 1) (make-symbol-node 'call/shift))
+                                      (list (at (l 1)
+                                                (make-lambda-node (list (at (l 3) (make-symbol-node 'kont)))
+                                                                  (at (l 4) (make-symbol-node 'expr)))))))))
 
  (it "reset macro works"
-     (assert (macro-expand '(reset expr) bm)
-             '(call/reset
-               (lambda () expr))))
+     (assert (expand-macros (at (l 1) (make-list-node
+                                       (list (at (l 2) (make-symbol-node 'reset))
+                                             (at (l 3) (make-symbol-node 'expr)))))
+                            bm)
+             (at (l 1) (make-app-node (at (l 1) (make-symbol-node 'call/reset))
+                                      (list (at (l 1)
+                                                (make-lambda-node '()
+                                                                  (at (l 3) (make-symbol-node 'expr)))))))))
 
  (it "letcc macro works"
-     (assert (macro-expand '(letcc k (k 23)) bm)
-             '(call/current-continuation
-               (lambda (k) (do (k 23)))))
-     (assert (macro-expand '(letcc k (k 23) (k 5)) bm)
-             '(call/current-continuation
-               (lambda (k) (do (k 23) (k 5)))))))
+     (assert (expand-macros (at (l 1) (make-list-node
+                                       (list (at (l 2) (make-symbol-node 'letcc))
+                                             (at (l 3) (make-symbol-node 'kont))
+                                             (at (l 4) (make-symbol-node 'expr)))))
+                            bm)
+             (at (l 1) (make-app-node (at (l 1) (make-symbol-node 'call/current-continuation))
+                                      (list (at (l 1)
+                                                (make-lambda-node (list (at (l 3) (make-symbol-node 'kont)))
+                                                                  (at (l 4) (make-symbol-node 'expr))))))))
+     (assert (expand-macros (at (l 1) (make-list-node
+                                       (list (at (l 2) (make-symbol-node 'letcc))
+                                             (at (l 3) (make-symbol-node 'kont))
+                                             (at (l 4) (make-symbol-node 'expr1))
+                                             (at (l 5) (make-symbol-node 'expr2)))))
+                            bm)
+             (at (l 1) (make-app-node (at (l 1) (make-symbol-node 'call/current-continuation))
+                                      (list (at (l 1)
+                                                (make-lambda-node (list (at (l 3) (make-symbol-node 'kont)))
+                                                                  (at (location 4 5)
+                                                                      (generated
+                                                                       (make-do-node
+                                                                        (list (at (l 4) (make-symbol-node 'expr1))
+                                                                              (at (l 5) (make-symbol-node 'expr2))))))))))))))
