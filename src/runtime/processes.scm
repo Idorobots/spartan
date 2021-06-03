afdfdb8a490f4575df97fbacbb24b7efc63176a2
Kajetan Rzepecki <k@spartan.works>
2021-06-03 21:42:37 +0200

    Refactored process model definition.

--- src/runtime/processes.scm
+++ src/runtime/processes.scm
@@ -3,77 +3,38 @@
 (load-once "compiler/utils/utils.scm")
 (load-once "runtime/continuations.scm")
 
-(define (&uproc fields)
-  (list &uproc fields))
+(struct uproc
+  ((continuation #:mutable)
+   (error-handler #:mutable)
+   (state #:mutable)
+   (rtime #:mutable)
+   priority
+   pid
+   (msg-queue #:mutable))
+  #:transparent
+  #:constructor-name make-uproc)
 
 (define (uproc priority cont handler rtime state)
-  (let ((f (array 7 '())))
-    (array-assign! f 0 cont)
-    (array-assign! f 1 rtime)
-    (array-assign! f 2 priority)
-    (array-assign! f 3 (gensym 'pid))
-    (array-assign! f 4 '()) ;; NOTE message queue
-    (array-assign! f 5 state)
-    (array-assign! f 6 handler)
-    (&uproc f)))
-
-(define (uproc? thing)
-  (tagged-list? &uproc thing))
-
-(define (uproc-continuation uproc)
-  (array-ref (cadr uproc) 0))
-
-(define (set-uproc-continuation! uproc cont)
-  (array-assign! (cadr uproc) 0 cont))
-
-(define (uproc-rtime uproc)
-  (array-ref (cadr uproc) 1))
-
-(define (set-uproc-rtime! uproc time)
-  (array-assign! (cadr uproc) 1 time))
+  (make-uproc cont handler state rtime priority (gensym 'pid) '()))
 
 (define (inc-uproc-rtime! uproc by)
   (set-uproc-rtime! uproc (+ by (uproc-rtime uproc))))
-
-(define (uproc-priority uproc)
-  (array-ref (cadr uproc) 2))
 
 (define (uproc-vtime uproc)
   (* (uproc-rtime uproc)
      (uproc-priority uproc)))
 
-(define (uproc-pid uproc)
-  (array-ref (cadr uproc) 3))
-
-(define (uproc-msg-queue uproc)
-  (array-ref (cadr uproc) 4))
-
 (define (uproc-enqueue-msg! uproc msg)
   ;; FIXME Gross...
-  (array-assign! (cadr uproc)
-                 4
-                 (append (uproc-msg-queue uproc)
-                         (list msg))))
+  (set-uproc-msg-queue! uproc
+                        (append (uproc-msg-queue uproc)
+                                (list msg))))
 
 (define (uproc-dequeue-msg! uproc)
   (let* ((q (uproc-msg-queue uproc))
          (msg (car q)))
-    (array-assign! (cadr uproc)
-                   4
-                   (cdr q))
+    (set-uproc-msg-queue! uproc (cdr q))
     msg))
 
 (define (uproc-msg-queue-empty? uproc)
   (empty? (uproc-msg-queue uproc)))
-
-(define (uproc-state uproc)
-  (array-ref (cadr uproc) 5))
-
-(define (set-uproc-state! uproc state)
-  (array-assign! (cadr uproc) 5 state))
-
-(define (uproc-error-handler uproc)
-  (array-ref (cadr uproc) 6))
-
-(define (set-uproc-error-handler! uproc handler)
-  (array-assign! (cadr uproc) 6 handler))
