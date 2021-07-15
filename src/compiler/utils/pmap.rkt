d931d33b3887652781b9e370fb7da5c7a3844b4c
Kajetan Rzepecki <k@spartan.works>
2021-07-15 22:33:23 +0200

    Added a thread-pool implementation of pmap.

--- src/compiler/utils/pmap.rkt
+++ src/compiler/utils/pmap.rkt
@@ -2,14 +2,27 @@
 
 (require racket/future)
 
-(provide pmap)
+(provide pmap
+         future-map
+         thread-pool-map
+         spawn-thread-pool)
 
-(define +parallel?+ #t)
+(define +parallelism+ 8)
+
+(define *thread-pool* #f)
 
 (define (pmap fun lst)
-  (if +parallel?+
-      (future-map fun lst)
-      (map fun lst)))
+  (cond ((= +parallelism+ 0)
+         (map fun lst))
+        ((= +parallelism+ 1)
+         (future-map fun lst))
+        (else
+         (thread-pool-map (or *thread-pool*
+                              (begin
+                                (set! *thread-pool* (spawn-thread-pool +parallelism+))
+                                *thread-pool*))
+                          fun
+                          lst))))
 
 (define (future-map fun lst)
   (map touch
@@ -18,3 +31,35 @@
                (lambda ()
                  (fun pass))))
             lst)))
+
+(define (thread-pool-map thread-pool fun lst)
+  ;; NOTE Assigns work in round-robin fashion.
+  (let loop ((values lst)
+             (threads *thread-pool*))
+    (cond ((null? threads)
+           (loop values *thread-pool*))
+          ((null? values)
+           '())
+          (else
+           (thread-send (car threads)
+                        (lambda ()
+                          (fun (car values))))
+           (loop (cdr values)
+                 (cdr threads)))))
+  ;; NOTE Doesn't care about the order at all.
+  (let loop ((values lst))
+    (if (null? values)
+        '()
+        (cons (thread-receive)
+              (loop (cdr values))))))
+
+(define (spawn-thread-pool threads)
+  (let ((t (current-thread)))
+    (map (lambda _
+           (thread
+            (lambda ()
+              (let loop ()
+                (let ((workload (thread-receive)))
+                  (thread-send t (workload))
+                  (loop))))))
+         (make-list threads #f))))
