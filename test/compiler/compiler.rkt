2ca6f451d4acdaa67e2c4ad65163bd289cc27c00
Kajetan Rzepecki <k@spartan.works>
2021-09-19 16:33:47 +0200

    Added separate tests for the superoptimizer.

--- test/compiler/compiler.rkt
+++ test/compiler/compiler.rkt
@@ -7,6 +7,7 @@
 (require "../../src/compiler/compiler.rkt")
 (require "../../src/compiler/utils/gensym.rkt")
 (require "../../src/compiler/utils/io.rkt")
+(require "../../src/compiler/passes/optimize.rkt")
 
 (describe
  "compiler"
@@ -20,7 +21,7 @@
                                         (path->string path)))
                        (directory-list "test/sprtn/errors/")))))
 
- (it "optimizes the output"
+ (it "optimizes the output naively"
      (assert (compile (env 'module "optimize"
                            'input (slurp "test/sprtn/math.sprtn")))
              '(display '(5 1462731 23)))
@@ -39,4 +40,30 @@
                                                        1
                                                        (* x (fact (- x 1)))))))
                                     (fact 2))"))
-             ''2)))
+             ''2))
+
+ (it "superoptimizes the output"
+     (assert (compile (env 'module "optimize"
+                           'optimize optimize-super
+                           'input (slurp "test/sprtn/math.sprtn")))
+             '(display '(5 1462731 23)))
+     (assert (compile (env 'module "optimize"
+                           'optimize optimize-super
+                           'input "(letrec ((q (lambda () 8))
+                                            (f (lambda (x) (+  x (q))))
+                                            (r (lambda () (f (q))))
+                                            (s (lambda () (+ (r) (f 2))))
+                                            (g (lambda () (+ (r) (s))))
+                                            (t (lambda () (g))))
+                                     (t))"))
+             ''42))
+
+ (ignore "superoptimizez recursive functions"
+         (assert (compile (env 'module "optimize"
+                               'optimize optimize-super
+                               'input "(letrec ((fact (lambda (x)
+                                                   (if (= 0 x)
+                                                       1
+                                                       (* x (fact (- x 1)))))))
+                                    (fact 2))"))
+                 ''2)))
