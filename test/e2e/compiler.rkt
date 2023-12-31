20075d6a34f7904677f2cd82026b43760d011743
Kajetan Rzepecki <k@spartan.works>
2023-12-31 18:35:23 +0100

    Cleaned up the compiler integration tests.

--- test/e2e/compiler.rkt
+++ test/e2e/compiler.rkt
@@ -3,11 +3,6 @@
 ;; Parse tree validation tests.
 
 (require "../testing.rkt")
-(require "../../src/compiler/ast.rkt")
-(require "../../src/compiler/env.rkt")
-(require "../../src/compiler/compiler.rkt")
-(require "../../src/compiler/utils/gensym.rkt")
-(require "../../src/compiler/passes/optimize.rkt")
 
 (describe
  "compiler"
